133382908af38236d720955e6b5267ff30b9ab3c
Resten <saup19@naver.com>
2020-02-10 14:49:29 +0900

    first_build

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import cat from './slave_nonsan.jpg'
+import stop from './stop_it.jpg'
 
 function App() {
   const countDownDate = new Date("March 19, 2020 11:00:00").getTime();
@@ -62,11 +63,11 @@ function App() {
         <span>{Math.floor((times.allTime  % (1000 * 60)) / 1000)} 초</span>  
       </div> */}
       <div style={{position: "relative"}}>
-        <div style={{position:"absolute",width:"100vw",top:"40vh"}}>
+        <div style={{position:"absolute",width:"100vw",paddingTop:"40vh"}}>
            <span style={{zIndex:100,color:"white",fontSize:"2.3rem",position:"absolute",textAlign:"center",width:"100%",lineHeight:"0%"}}>전역까지</span>
-           <span style={{zIndex:100,color:"white",fontSize:"11rem",position:"absolute",textAlign:"center",width:"100%",lineHeight:"120%"}}>{date(times)}</span>
+           <span style={{zIndex:100,color:"white",fontSize:"9.5rem",position:"absolute",textAlign:"center",width:"100%",lineHeight:"120%"}}>{date(times)}</span>
         </div>
-        <img src={cat} alt={"123"} style={{width: "100vw",height:"100vh",position:"absolute",left:0,}}/>
+        <img src={stop} alt={"123"} style={{width: "100vw",height:"100vh",position:"absolute",left:0,}}/>
         <div style={{width:"100vw",height:"100vh",backgroundColor:"black",position:"absolute",opacity:"50%",}}/>
         
        
