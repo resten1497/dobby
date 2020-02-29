747b1748ea857d8fe5370e80aa5b52b90ad65916
Pinot. Kim <sohuyn43@gmail.com>
2020-03-01 02:01:11 +0900

    ♻️: 코드를 보기 좋게 수정
    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, {useState, useEffect} from 'react';
 import style from "./css/App.module.css";
 
 function App() {
+    const now = new Date();
+
     const countDownDate = new Date("March 19, 2020 11:00:00").getTime();
     const countDownNonsanDate = new Date("February 18, 2020 14:00:00").getTime();
     const countDownAllDate = new Date(" December 18, 2021 00:00:00").getTime();
-
-    const now = new Date();
 
     const [times, setTimes] = useState({
         time: countDownDate - now.getTime(),
@@ -15,22 +15,25 @@ function App() {
     });
 
     useEffect(() => {
-        let test = setInterval(() => {
-            let now = Date.now();
-            let distance = countDownDate - now;
-            let alldistance = countDownAllDate - now;
-            let Nonsandistance = countDownNonsanDate - now;
+        const test = setInterval(() => {
+            const now = Date.now();
+
+            const distance = countDownDate - now;
+            const alldistance = countDownAllDate - now;
+            const Nonsandistance = countDownNonsanDate - now;
+
             setTimes({
                 time: distance,
                 allTime: alldistance,
                 nonsanTime: Nonsandistance,
-            })
-
+            });
         }, 1000);
 
         return () => {
             clearInterval(test)
         }
+
+        // eslint-disable-next-line
     }, []);
 
     return (
@@ -47,9 +50,9 @@ function App() {
 
 
 const date = (times) => {
-    return `${Math.floor(times.time / (1000 * 60 * 60 * 24))}일 
-            ${Math.floor((times.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}시간 
-            ${Math.floor((times.time % (1000 * 60 * 60)) / (1000 * 60))}분 
+    return `${Math.floor(times.time / (1000 * 60 * 60 * 24))}일
+            ${Math.floor((times.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}시간
+            ${Math.floor((times.time % (1000 * 60 * 60)) / (1000 * 60))}분
             ${Math.floor((times.time % (1000 * 60)) / 1000)}초`;
 };
 
