e92cc3f08c28f7f88f461e754b6e760db9bcca34
Resten1497 <=>
2021-11-12 13:17:28 +0900

    fix:change discrition

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
 
     return (
         <div className={style.App}>
-            <img src="images/slave_nonsan.jpg" alt="이곳은 호국 요람의 도시, 논산입니다." className={style.background} />
+            <img src="images/slave_nonsan.jpg" alt="똑바로서라핫산" className={style.background} />
 
             <div className={style.text}>
                 <h3 className={style.title}>전역까지</h3>
